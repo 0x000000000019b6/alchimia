11722045d9a224ca0885a5afc7c8599316622d08
0x000000000019b6 <77126080+0x000000000019b6@users.noreply.github.com>
2022-06-08 14:09:27 +0200

    Update strategy.py

--- alchimia/strategy.py
+++ alchimia/strategy.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division
 
-from sqlalchemy.engine.strategies import DefaultEngineStrategy
+from sqlalchemy.engine.strategies import MockEngineStrategy
 
 from alchimia.engine import TwistedEngine
 
@@ -8,7 +8,7 @@ from alchimia.engine import TwistedEngine
 TWISTED_STRATEGY = "_twisted"
 
 
-class TwistedEngineStrategy(DefaultEngineStrategy):
+class TwistedEngineStrategy(MockEngineStrategy):
     """
     An EngineStrategy for use with Twisted. Many of the Engine's methods will
     return Deferreds instead of results. See the documentation of
